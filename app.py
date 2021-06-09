a658186f1e8b399ee115fff8a73fe2636174b7e9
Igor <erziak.i@gmail.com>
2021-06-09 12:31:39 +0200

    add basic auth

--- app.py
+++ app.py
@@ -1,8 +1,22 @@
-from flask import Flask
+from flask import Flask, abort
+from flask_httpauth import HTTPBasicAuth
+from os import environ
 
 app = Flask(__name__)
+auth = HTTPBasicAuth()
+
+creds = {
+    "username": environ.get("ROSSUM_USERNAME"),
+    "password": environ.get("ROSSUM_PASSWORD")
+}
+
+@auth.verify_password
+def verify_password(username, password):
+    if username == creds['username'] and password == creds['password']:
+        return username
 
 @app.route("/export")
+@auth.login_required
 def export():
-    return "XML file here"
+    return f"XML file for user {auth.current_user()}"
 
