c20e4308909477f714e0283d45d00c058ef6016d
Igor <erziak.i@gmail.com>
2021-06-09 12:43:13 +0200

    accept annotation ID as a parameter in the path

--- app.py
+++ app.py
@@ -5,18 +5,22 @@ from os import environ
 app = Flask(__name__)
 auth = HTTPBasicAuth()
 
-creds = {
+CREDS = {
     "username": environ.get("ROSSUM_USERNAME"),
     "password": environ.get("ROSSUM_PASSWORD")
 }
+ANNOTATION_ID = 123456
 
 @auth.verify_password
 def verify_password(username, password):
-    if username == creds['username'] and password == creds['password']:
+    if username == CREDS['username'] and password == CREDS['password']:
         return username
 
-@app.route("/export")
+@app.route("/export/<int:annotation_id>")
 @auth.login_required
-def export():
-    return f"XML file for user {auth.current_user()}"
+def export(annotation_id):
+    if annotation_id == ANNOTATION_ID:
+        return f"Success for user {auth.current_user()} and annotation ID {annotation_id}"
+    else:
+        return f"Wrong annotation id for user {auth.current_user()}"
 
