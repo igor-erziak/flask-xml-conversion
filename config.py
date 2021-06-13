6acc96833ea96062ff1f9dd3dae19cecb5faf320
Igor Erziak <erziak.i@gmail.com>
2021-06-14 01:30:05 +0200

    simplify config

--- config.py
+++ config.py
@@ -1,11 +1,6 @@
 from os import environ
-import sys
 
-try:
-    ROSSUM_USERNAME = environ['ROSSUM_USERNAME']
-    ROSSUM_PASSWORD = environ['ROSSUM_PASSWORD']
-    APP_USERNAME = environ["APP_USERNAME"]
-    APP_PASSWORD = environ["APP_PASSWORD"]
-except KeyError as e:
-    print(f'KeyError: the environment variable {e} is not set.')
-    sys.exit('Terminating...')
+ROSSUM_USERNAME = environ['ROSSUM_USERNAME']
+ROSSUM_PASSWORD = environ['ROSSUM_PASSWORD']
+APP_USERNAME = environ["APP_USERNAME"]
+APP_PASSWORD = environ["APP_PASSWORD"]
